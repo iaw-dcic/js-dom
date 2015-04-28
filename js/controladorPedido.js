b1b060e5ba6c370648999778f20ea9814898dd47
MarianoTucat <mariano@skedgo.com>
2015-04-28 15:07:21 -0300

    usando ajax

--- js/controladorPedido.js
+++ js/controladorPedido.js
@@ -1,16 +1,19 @@
-var jsonPizzas;
+var pedido;
 
 $(function() {
-    var pizzas = ordenarPizzas();
-    mostrarPizzas(pizzas);
+    var ajax = new AJAXInteraction("./data/pizzas.json", function(data) {
+        pedido = data;
+        mostrarPizzas(ordenarPizzas(data));
+    });
+    ajax.doGet();
 });
 
-function ordenarPizzas() {
+function ordenarPizzas(data) {
     var index;
 
     var pizzas = new Object();
-    for (index = 0; index < jsonPizzas.length; ++index) {
-        var pizza = jsonPizzas[index];
+    for (index = 0; index < data.length; ++index) {
+        var pizza = data[index];
         pizza.id = index;
         pizza.enPedido = false;
         var pizzasPorTipo;
@@ -32,7 +35,7 @@ function actualizarPedido(e) {
     }
     var id = target.getAttribute("id");
     
-    var pizza = jsonPizzas[id];
+    var pizza = pedido[id];
     pizza.enPedido = !pizza.enPedido;
 
     //le cambio el estado
@@ -48,11 +51,39 @@ function actualizarPedido(e) {
 function computarTotal() {
     var total = 0.0;
 
-    for (index = 0; index < jsonPizzas.length; ++index) {
-        var pizza = jsonPizzas[index];
+    for (index = 0; index < pedido.length; ++index) {
+        var pizza = pedido[index];
         if (pizza.enPedido) {
             total += pizza.precio;
         }
     }
     return total;
+}
+
+function AJAXInteraction(url, callback) {
+
+    var req = inicializar();
+    req.onreadystatechange = procesarRequest;
+
+    function inicializar() {
+        if (window.XMLHttpRequest) {
+            return new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    }
+
+    function procesarRequest() {
+        if (req.readyState === 4) { // completo ! !
+            if (req.status === 200) { // exito ! ! 
+                if (callback)
+                    callback(JSON.parse(req.responseText));
+            }
+        }
+    }
+
+    this.doGet = function() {
+        req.open("GET", url, true);
+        req.send(null);
+    };
 }
